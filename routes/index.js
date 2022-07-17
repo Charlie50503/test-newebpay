df977acf50654c3634b67e7f4133c2371f176c1c
charlie50503 <cate50503@gmail.com>
2022-07-18 00:18:21 +0900

    第一版

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 const orders={}
 /* GET home page. */
+
+const RespondType = 'JSON'
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/createOrder' , (req,res)=>{
+router.post('/createOrder' , (req,res)=>{
+  const data = req.body;
+  console.log(data);
   res.json()
 })
 module.exports = router;
